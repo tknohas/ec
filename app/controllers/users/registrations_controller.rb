2d0ba2deaf218203e8ddbe5c59a72eab1feaedd9
{tknohas} <0xv17851726325v@gmail.com>
2025-01-13 12:08:26 +0900

    fix a bit

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,6 +8,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
   end
 end
