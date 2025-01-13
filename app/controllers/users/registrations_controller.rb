9adbfac052170fb7480ff7050f86883c5eb2de33
{tknohas} <0xv17851726325v@gmail.com>
2025-01-13 12:05:46 +0900

    ユーザーがログインできるように

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters
+
+  def after_sign_up_path_for(_resource)
+    root_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-# Admin.create!(email: 'admin@example.com', password: 'password')
-names = %w(にんじん ピーマン 玉ねぎ バナナ アボカド)
-prices = [100, 1_000, 10_000, 200, 2_000]
-(1..names.length).each do |n|
-  Product.create!(
-    name: names[n - 1],
-    price: prices[n - 1],
-    description: '商品説明です。',
-    position: n
-  )
-end
+Admin.create!(email: 'admin@example.com', password: 'password')
+# names = %w(にんじん ピーマン 玉ねぎ バナナ アボカド)
+# prices = [100, 1_000, 10_000, 200, 2_000]
+# (1..names.length).each do |n|
+#   Product.create!(
+#     name: names[n - 1],
+#     price: prices[n - 1],
+#     description: '商品説明です。',
+#     position: n
+#   )
+# end
